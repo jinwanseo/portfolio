553bdbd91ce7912e851f9c5dfb2a563e50315441
jinwanseo <jinwanseo@gmail.com>
2021-04-15 23:46:47 +0900

    Js : Home

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Menubar 
 //메뉴 바 스크롤
 document.addEventListener('scroll',e=>{
     let navbar = document.querySelector('#navbar');
@@ -10,6 +11,24 @@ document.addEventListener('scroll',e=>{
 //메뉴바 클릭시 이동
 document.querySelector('.navbar__menu').addEventListener('click',e=>{
     if(e.target.nodeName === 'LI'){
-        document.querySelector(e.target.dataset.link).scrollIntoView({behavior : 'smooth'});
+        scrollLink(e.target.dataset.link);
     }
 });
+
+//Home
+//contactme 버튼 클릭시
+document.querySelector('.home__contact').addEventListener('click',e=>{
+    scrollLink('#contact');
+});
+
+//스크롤시 점점 옅게
+window.addEventListener('scroll',e=>{
+    let elem_height = document.querySelector('#home').getBoundingClientRect().height;
+    let opacity_value = ((elem_height-window.scrollY)/elem_height);
+    document.querySelector('.home__container').style.opacity = opacity_value;
+});
+
+//scroll down func
+const scrollLink = (selector)=>{
+    document.querySelector(selector).scrollIntoView({behavior:'smooth'});
+}
